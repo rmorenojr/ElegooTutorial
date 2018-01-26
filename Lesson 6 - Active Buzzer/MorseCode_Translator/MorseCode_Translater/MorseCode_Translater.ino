c0b33e382d17fd94357a55fff3bc3f9599758357
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-01-25 18:00:01 -0700

    Lesson 6 - Active Buzzer and Morse Code Translator
    Lesson 6 – Active Buzzer and Morse Code Translator
    
    A beginner's guide to the Most Complete Starter Kit by Elegoo.  Lesson 6 – Active Buzzer

---
+++ Lesson 6 - Active Buzzer/MorseCode_Translator/MorseCode_Translater/MorseCode_Translater.ino
@@ -0,0 +1,238 @@
+/* ***********************************************************
+ *  Morse Code Translater
+ *  By Ricardo Moreno Jr. 
+ *  created 01/15/2018
+ *  
+ *  Inspired by learnelectronics
+ *  https://www.youtube.com/watch?v=3WjNiJOcVaM
+ *  Nick B 
+ *  https://pastebin.com/SM9jmjdG
+ *  
+ *  From Wikipedia
+ *  length of a dot is 1 unit
+ *  length of a dash is 3 units
+ *  length of a space between parts of the same letter is 1 unit
+ *  length of a space between letters is 3 units
+ *  length of a space between words is seven units
+ *  
+ *  The Dot or Dash are commonly referred to as Dit or Dah
+ * *********************************************************** */
+
+//Global Constants Other variables
+const int buzzerPin = 12;          //set buzzer pin
+const int ledPin = 6;              //set LED pin
+
+//Global Variables
+String strInput = ""; 
+  
+//Morse Code Specific delays
+int mcUnit = 75;                    //length of time in milliseconds
+                                    //   mcUnit is the only value that
+                                    //   needs adjusting
+int dit = mcUnit;                   //dit equals 1 unit of time
+int dah = 3 * mcUnit;               //dah equals 3 units of time
+int partsDelay = mcUnit;            //1 unit between parts 
+int letterDelay = 3 * mcUnit;       //3 units between letters
+int wordDelay = 7 * mcUnit;         //7 units between words
+
+/* ***********************************************************
+ *                         Global Arrays
+ * *********************************************************** */
+ //Recall Arrays start from 0
+String letters[] = {
+  ".-", "-...", "-.-.", "-..",      // A-D
+  ".", "..-.", "--.", "....",       // E-H
+  "..", ".---", "-.-", ".-..",      // I-L
+  "--", "-.", "---", ".--.",        // M-P
+  "--.-", ".-.", "...", "-",        // Q-T
+  "..-", "...-", ".--", "-..-",     // U-X
+  "-.--", "--.."                    // Y-Z
+};
+
+String numbers[] = {
+  "-----", ".----", "..---",       //0-2 
+  "...--", "....-", ".....",       //3-5
+  "-....", "--...", "---..",       //6-8
+  "----."};                        //9
+
+String Lpunctuation[] = {
+  "-.-.--",   //! -33
+  ".-..-.",   //" -34
+  "X",        //# -35
+  "...-..-",  //$ -36
+  "X",        //% -37  
+  ".-...",    //& -38
+  ".----.",   //' -39
+  "-.--.",    //( -40
+  "-.--.-",   //) -41
+  "X",        //* -42
+  ".-.-.",    //+ -43
+  "X",        //, -44
+  "-....-",   //- -45
+  ".-.-.-",   //. -46
+  "-..-.",    /// -47
+};
+
+String Upunctuation[] = {
+  "---...",   //: -58
+  "-.-.-.",   //; -59
+  "X",        //< -60
+  "-...-",    //= -61
+  "X",        //> -62
+  "..--..",   //? -63
+  ".--.-."    //@ -64
+};
+
+String SpecialCase[] = {
+    "_", "..--.-",   //_ -95
+};
+
+//Not included yet
+String ProSigns[] = {
+  "...-.-",   //End Of Work
+  "........", //Error
+  "-.-",      //Invitation to Transmit - K
+  "-.-.-",    //Starting Signal
+  ".-.-.",    //New Page Signal - +
+  "...-.",    //Understood  - also Ŝ
+  ".-..."     //Wait - also &
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);                       //Used for Serial Monitor
+  pinMode(buzzerPin, OUTPUT);               //set buzzer pin, OUTPUT automatically sets to LOW
+  pinMode(ledPin, OUTPUT);                  //set LED pin, OUTPUT automatically sets to LOW
+}
+
+void loop(){
+  // put your main code here, to run repeatedly:
+  Serial.println("Enter a string you want to translate to Morse Code");
+
+  //Use the Serial Monitor to input a message
+  while (Serial.available() == 0){
+    //do nothing                            //Wait until user input
+  }
+  strInput = Serial.readString();           //get the new String
+  Serial.print(" Translating => ");
+  Serial.println(strInput);                 //print string to serial monitor
+  doString(strInput);                       //go to doString function
+
+  delay(1000);                              //delay between loops
+  Serial.println("");                       //Add a new line feed to serial monitor   
+  strInput="";                              //clear the string
+}
+
+/* ***********************************************************
+ *                Functions - Order is Important
+ * *********************************************************** */
+void DitOrDah(int DitOrDahDelay){
+  //This function turns ON and OFF the LED
+  //  and Active Buzzer by the delay value
+  //  passed to it.
+  digitalWrite(buzzerPin, HIGH);
+  digitalWrite(ledPin, HIGH);
+  delay(DitOrDahDelay);                    //dit or dah delay
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(ledPin, LOW);
+}
+
+void doSequence(String sequence){
+  //Local Variables
+  int strLen = 0;
+  char chValue = "";
+  
+  strLen = sequence.length();               //get length of string
+  //note: last char is a NULL
+  for (int x = 0; x < strLen; x++){        //loop through all parts
+    chValue = sequence.charAt(x);           //get char value
+    if(chValue == '.'){
+      DitOrDah(dit);                        // do a Dit
+      Serial.print(".");                    // print dit
+    }
+    else if (chValue == '-'){
+      DitOrDah(dah);                        // do a Dah
+      Serial.print("-");                    // print dah
+    }
+    else if (chValue == 'X'){               // No translation
+      Serial.print("X - no translation");   // print no translation 
+    }
+    else if (int(chValue) == 0){            // Null char in string
+      //Null found                          // May not be needed
+      //Do Nothing                          // should be skipped
+    } //end if statement
+
+    if (x < strLen) delay(partsDelay);      // gap between parts
+  } //end for
+  Serial.println("");
+} //end doSequence
+
+void translate(char ch){
+/* Recall that letters have a numeric ASCII value
+ * so you can add and substract them
+ * a = 97
+ * A = 65
+ * 0 = 48
+ * 
+ * Modify this function for additonal character checks 
+ * ASCII numerical order below (mostly)
+ */
+  if (ch >= '!' && ch <= '/'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(Lpunctuation[ch - '!']);    
+  } 
+  else if (ch >= '0' && ch <= '9'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(numbers[ch - '0']);
+  }
+  else if (ch >= ':' && ch <= '@'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(Upunctuation[ch - ':']);    
+  } 
+  else if (ch >= 'A' && ch <= 'Z'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(letters[ch - 'A']);
+  }
+  else if (ch >= 'a' && ch <= 'z'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(letters[ch - 'a']);
+  }
+  else if (ch == '_'){
+    Serial.print(ch);
+    Serial.print(" ");
+    doSequence(SpecialCase[1]);    
+  }
+  else {
+    Serial.println("invalid character");
+    //Skip invalid character
+  } //end if statement
+} //end translate
+
+void doString(String strValue){
+  char chValue;                               //Local Variables
+  int strLen = 0;
+
+  strLen = strValue.length();                 //get the length of the string
+
+  for(int x = 0; x <= strLen; x++){           //loop through string to get each value
+    chValue = strInput.charAt(x);             //get char at position x
+ 
+    if (chValue == " " || int(chValue) == 32){//Found Space between words
+      Serial.println("(space)");
+      delay(wordDelay);                        //delay between words
+    } else if (int(chValue) == 13){            //Carrige Return found
+      //Do Nothing
+    } else if (int(chValue) == 0){             //Null found
+      //Do Nothing
+    } else {
+      translate(chValue);                      //translate character to Morse Code
+      if (x < strLen) delay(letterDelay);      //delay between letters
+    } //end if
+  } //end for
+} //end doString
+
---
+++ Lesson 6 - Active Buzzer/README.md
@@ -0,0 +1,64 @@
+# ElegooTutorial
+
+TITLE:  Lesson 6 – Active Buzzer and Morse Code Translator
+Video Located here: https://youtu.be/RKejZWnCWx8
+
+A beginner's guide to the Most Complete Starter Kit by Elegoo.  Lesson 6 – Active Buzzer
+
+Hello everyone, I'm Rick the Tech Enthusiast again, with the next Lesson number 6 “Active Buzzer”.  This is the next lesson on my Arduino UNO Series covering the Most Complete Starter Kit by Elegoo.   I try to follow the tutorial closely, without overburdening the viewer with every detail, but I will point out any changes or corrections as I find them.
+
+As mentioned before, I purchased this Elegoo starter kit.  I’m pronouncing it El-E-Goo, I hope that’s right (I sometimes slip and say El-a-Goo).  No, Elegoo isn't sponsoring me.  I just thought it would be fun to do a video of each Lesson as I was learning the Arduino environment. 
+
+For this video, we’ll need a few things from your Elegoo Arduino kit.  I’ll list the items below.  In this lesson we’re going to learn what an active buzzer is and how to use them in our projects.  I step through the Elegoo provided code to explain how it makes this happen.  The pulsating tone makes a great pet scaring circuit.  And as I was just about to finish the video, I got this idea on making a Morse Code Translator circuit.  The Morse Code Translator circuit used the active buzzer, LED, couple of resistors and a few jumpers to the Arduino.  The code is a little more advanced, so I’ve included some additional links below that covers some of the topics.  Now, I have to admit that I use my phone to record all the live video shots, and I have to sync them up in my editor.  Which is Movie Maker for Windows (cue the sad trombone).  It was a challenge to mesh the screen capture and live capture together. But I assure you that the code works.  I like to thank learnelectronics and Nick B for inspiring me to create the Morse Code Sketch.  And the Programming Electronics Academy has a great video “An easy way to make noise with Arduino using tone()”,  which may apply more towards the next lesson but he has a fantastic piezo buzzer explanation.  
+
+Of course, I’ve added Serial.begin, Serial.print, and Serial.read statements to view and diagnose the code in the Serial Monitor.  I hope you like the Lesson, it was a lot of fun to make.
+
+Parts you’ll need:
+Arduino UNO R3 board
+Active Buzzer (that the one with a white sticker on it)
+Two female-to-male wire jumpers
+
+The next lesson will be Lesson 7 – Passive Buzzer  
+Links:
+This lesson’s sketch and associated drawings can be found at Github:
+https://github.com/rmorenojr/ElegooTutorial
+ 
+Elegoo on Amazon’s web site:
+https://www.amazon.com/gp/product/B01CZTLHGE/ref=oh_aui_detailpage_o06_s00?ie=UTF8&psc=1
+Elegoo’s web site:
+https://www.elegoo.com/
+https://www.elegoo.com/product/elegoo-uno-r3-project-complete-starter-kit/
+
+Frtizing Software:
+http://fritzing.org/download/
+
+Arduino Language Reference (English):
+https://www.arduino.cc/reference/en/
+https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
+Arduino ASCII chart
+https://www.arduino.cc/en/Reference/ASCIIchart
+
+Morse Code - Wikipedia.org
+https://en.wikipedia.org/wiki/Morse_code
+
+Arduino Morse Code Translator - by learnelectronics
+https://www.youtube.com/watch?v=3WjNiJOcVaM
+
+Arduino Morse Code Translator - Nick B 
+https://pastebin.com/SM9jmjdG
+ 
+Great piezo buzzer explanation:
+An easy way to make noise with Arduino using tone() - Programming Electronics Academy
+https://www.youtube.com/watch?v=1_LMAgO14z0&t
+
+LESSON 12: Simple and Easy Way to Read Strings Floats and Ints over Arduino Serial Port
+Paul McWhorter
+https://www.youtube.com/watch?v=VIa_QZWIonQ
+
+Music provided by Free Music Archive, by artist Jahzzar
+Jahzzar_-_07_-_maniac
+http://freemusicarchive.org/music/Jahzzar/
+
+This is the seventh video on a new Arduino tutorial series. If you like the series, be sure to rate and subscribe.
+
+Thanks for watching
